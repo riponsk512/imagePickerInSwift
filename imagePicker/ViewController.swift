2c7cee0f2d1566d2cde1be84dc3012dd7f0f7fea
Ripon Sk <riponsk477@gmail.com>
2022-10-13 22:15:33 +0530

    commit all

--- imagePicker/ViewController.swift
+++ imagePicker/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageview: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(imageTap))
+        imageview.isUserInteractionEnabled = true
+        imageview.addGestureRecognizer(gesture)
+    }
+    
+    @objc func imageTap(){
+        let imgController = UIImagePickerController()
+        imgController.delegate = self
+        imgController.allowsEditing = true
+        imgController.sourceType = .photoLibrary
+        self.present(imgController, animated: true)
     }
 
 
+}
+extension ViewController:UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let img = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+            imageview.image = img
+            self.dismiss(animated: true)
+            
+        }
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true)
+    }
+    
 }
 
